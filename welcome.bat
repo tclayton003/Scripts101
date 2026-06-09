963bdf089a6c714b29115dc14b39d6770c88e334
tclayton003 <tclayton2003@gmail.com>
2026-06-08 21:16:04 -0500

    update: better OS version display on welcome.bat

--- welcome.bat
+++ welcome.bat
@@ -1,4 +1,8 @@
 @echo off
 Rem This file prints a welcome message to the user. It displays username, date & time, and OS version to the console.
 cls
-echo Welcome, %USERNAME%! It's currently %TIME% on %DATE%. The OS version is %OS%.
+Rem Capture the output of 'ver' in a variable to display the current OS version.
+for /f "delims=" %%i in ('ver') do (
+    set "os_version=%%i"
+)
+echo Welcome, %USERNAME%! It's currently %TIME% on %DATE%. You are currently running %os_version%.

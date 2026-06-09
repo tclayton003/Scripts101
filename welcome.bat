14fd29acbce6a9251c3098fac123a69df8e34dfd
tclayton003 <tclayton2003@gmail.com>
2026-06-08 20:54:40 -0500

    fix: @ symbol missing in welcome.bat

--- welcome.bat
+++ welcome.bat
@@ -1,3 +1,4 @@
-&commat;echo off
+@echo off
 Rem This file prints a welcome message to the user. It displays username, date & time, and OS version to the console.
+cls
 echo Welcome, %USERNAME%! It's currently %TIME% on %DATE%. The OS version is %OS%.

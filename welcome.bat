9b89dda8a02a55ccec04825e894ff0e0f21c7802
tclayton003 <tclayton2003@gmail.com>
2026-06-08 20:50:03 -0500

    created welcome.bat

---
+++ welcome.bat
@@ -0,0 +1,3 @@
+&commat;echo off
+Rem This file prints a welcome message to the user. It displays username, date & time, and OS version to the console.
+echo Welcome, %USERNAME%! It's currently %TIME% on %DATE%. The OS version is %OS%.

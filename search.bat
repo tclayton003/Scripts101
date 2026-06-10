accbcb0201c55db64d300776f0d2c8a227a9c18b
tclayton003 <tclayton2003@gmail.com>
2026-06-10 17:11:18 -0500

    update search.bat

--- search.bat
+++ search.bat
@@ -5,13 +5,29 @@ echo Please enter a valid file path to search. Example: C:\Users\%USERNAME%\Docu
 set /p search_dir=
 Rem Check if file path is valid.
 if exist "%search_dir%" (
-    echo %search_dir% is a valid path. Please enter a keyword to search.
+    echo %search_dir% is a valid path. Please enter a keyword to search. Example: notes
 ) else (
     echo %search_dir% is not a valid path. Try again.
     exit /b
 )
+Rem Ensure keyword and file_type are clear by setting them explicitly. This prevents bugs if they are left blank.
+set "keyword="
 set /p keyword=
-echo Searching %search_dir% for files containing "%keyword%" in their title...
-Rem Recursive search using directory and keyword.
-dir "%search_dir%" /a-d /b /s | find /i "%keyword%"
+Rem Check for no keyword.
+if "%keyword%" == "" (
+    echo A keyword is required to search. Try again.
+    exit /b
+)
+echo Please enter a file extension to search. Leave blank to search all file types. Example: .pdf
+set "file_type="
+set /p file_type=
+if "%file_type%" == "" (
+    echo Searching %search_dir% for files containing "%keyword%" in their title path...
+    Rem Recursive search using directory and keyword.
+    dir "%search_dir%" /a-d /s /b | find /i "%keyword%"
+) else (
+    echo Searching %search_dir% for %file_type% files containing "%keyword%" in their title path...
+    Rem Recursive search using directory, keyword, and file type.
+    dir "%search_dir%" /a-d /s /b | find /i "%keyword%" | find /i "%file_type%"
+)
 echo No more results.
